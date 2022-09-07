6db9481467ec33637a8397a667f1ff13669c43fb
Karl Chvojka <karl.chvojka@protonmail.com>
2022-09-07 15:57:52 -0400

    Initial commit

---
+++ src/StyledApp.js
@@ -0,0 +1,45 @@
+// Library Imports
+import styled from 'styled-components'
+import media from 'globalConfig/mobile'
+
+const StyledApp = styled.section`
+/* MOBILE FIRST DEFAULT STYLES */
+  width: 100%;
+
+/* Min width of 375 */
+  ${media.iphoneSe `
+    
+  `}
+
+/* Min width of 414 */
+  ${media.iphoneXr `
+    
+  `}
+
+/* Min width of 576 */
+  ${media.landscapePhones `
+    
+  `}
+
+/* Min width of 768 */
+  ${media.tablet `
+    
+  `}
+
+/* Min width of 1024 */
+  ${media.desktop `
+    
+  `}
+
+/* Min width of 1200 */
+  ${media.widescreen `
+    
+  `}
+
+/* Min width of 1980 */
+  ${media.udh `
+    
+  `}
+`
+
+export default StyledApp
